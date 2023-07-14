373cb00457df858cc6b9e016e32e4e40ee0e99ea
Peiyi Xu <xu_py311@outlook.com>
2023-07-13 22:47:06 -0400

    finished chapter 2

--- chapter2/dags/rocket.py
+++ chapter2/dags/rocket.py
@@ -7,12 +7,6 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-dag = DAG(
-    dag_id="download_rocket_launches",
-    start_date=airflow.utils.dates.date_ago(14),
-    schedule_interval=None 
-)
-
 
 def _get_pictures():
     # Ensure directory exists
@@ -43,3 +37,30 @@ def _get_pictures():
             print(f"Could not connect to {image_url}.")
  
  
+dag = DAG(
+    dag_id="download_rocket_launches",
+    start_date=airflow.utils.dates.days_ago(14),
+    schedule_interval="@daily" 
+)
+
+
+download_launches = BashOperator(
+    task_id="download_launches",
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
+    dag=dag
+)
+
+get_pictures = PythonOperator(
+    task_id="get_pictures",
+    python_callable=_get_pictures,
+    dag=dag
+)
+
+notify = BashOperator(
+    task_id="notify",
+    bash_command='echo "There are now $(ls /tmp/images/ | wc -l) images."',
+    dag=dag
+)
+
+download_launches >> get_pictures >> notify
+
